aa19264c53e5a788666ab6e5c6be2ed9f416c5b4
386592336@qq.com <386592336@qq.com>
2019-07-05 17:58:27 +0800

    MV页面部分完善

--- pages/mv/mv.js
+++ pages/mv/mv.js
@@ -1,117 +1,208 @@
 // pages/mv/mv.js
 var baseUrl = require('../../utils/api.js');
+const util = require('../../utils/util.js');
 Page({
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
-        // MV数据信息
-        mvDetail: [],
-        // MVUrl
-        mvUrl: [],
-    },
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    // 简介状态
+    isOpen: false,
+    // MV数据信息
+    mvDetail: [],
+    // MVUrl
+    mvUrl: [],
+    // 相关视频
+    relatedAllvideo: [],
+    // 精彩评论
+    hotComments: [],
+    // 最新评论
+    comments: [],
+    // 评论总数
+    total: 0
+  },
 
-    /**
-     * 获取MV数据
-     */
-    getMvDetail(id) {
-        let that = this;
-        wx.request({
-            url: baseUrl + 'mv/detail?mvid=' + id,
-            header: {
-                'Content-Type': 'application/json'
-            },
-            success: function(res) {
-                // console.log(res);
-                if (res.data.code == 200) {
-                    that.setData({
-                        mvDetail: res.data.data
-                    })
-                }
+  /**
+   * 获取MV数据
+   */
+  getMvDetail(id) {
+    let that = this;
+    wx.request({
+      url: baseUrl + 'mv/detail?mvid=' + id,
+      header: {
+        'Content-Type': 'application/json'
+      },
+      success: function(res) {
+        // console.log(res);
+        if (res.data.code == 200) {
+          that.setData({
+            mvDetail: res.data.data
+          })
+        }
+      }
+    })
+  },
+
+  /**
+   * 获取MVUrl
+   */
+  getMvUrl(id) {
+    let that = this;
+    wx.request({
+      url: baseUrl + 'mv/url?id=' + id,
+      header: {
+        'Content-Type': 'application/json'
+      },
+      success: function(res) {
+        // console.log(res);
+        if (res.data.code == 200) {
+          that.setData({
+            mvUrl: res.data.data
+          })
+        }
+      }
+    })
+  },
+
+  /**
+   * 获取相关视频
+   */
+  getRelatedAllvideo(id) {
+    let that = this;
+    wx.request({
+      url: baseUrl + 'related/allvideo?id=' + id,
+      header: {
+        'Content-Type': 'application/json'
+      },
+      success: function(res) {
+        // console.log(res);
+        if (res.data.code == 200) {
+          that.setData({
+            relatedAllvideo: res.data.data
+          })
+        }
+      }
+    })
+  },
+
+  /**
+   * 获取评论
+   */
+  getComments(id) {
+    let that = this;
+    wx.request({
+      url: baseUrl + 'comment/mv?id=' + id,
+      header: {
+        'Content-Type': 'application/json'
+      },
+      success: function(res) {
+        // console.log(res);
+        if (res.data.code == 200) {
+          // 评论表情需要转换(!!!)
+          var data = res.data;
+          for (let i in data.hotComments) {
+            data.hotComments[i].time = util.formatTime(data.hotComments[i].time, 2);
+            data.hotComments[i].content = util.emoji(data.hotComments[i].content)
+            if (data.hotComments[i].beReplied[0]) {
+              data.hotComments[i].beReplied[0].content = util.emoji(data.hotComments[i].beReplied[0].content)
             }
-        })
-    },
-
-    /**
-     * 获取MVUrl
-     */
-    getMvUrl(id) {
-        let that = this;
-        wx.request({
-            url: baseUrl + 'mv/url?id=' + id,
-            header: {
-                'Content-Type': 'application/json'
-            },
-            success: function(res) {
-                // console.log(res);
-                if (res.data.code == 200) {
-                    that.setData({
-                        mvUrl: res.data.data
-                    })
-                }
+          }
+          for (let i in data.comments) {
+            data.comments[i].time = util.formatTime(data.comments[i].time, 2);
+            data.comments[i].content = util.emoji(data.comments[i].content)
+            if (data.comments[i].beReplied[0]) {
+              data.comments[i].beReplied[0].content = util.emoji(data.comments[i].beReplied[0].content)
             }
-        })
-    },
+          };
+          that.setData({
+            hotComments: data.hotComments,
+            comments: data.comments,
+            total: data.total
+          })
+          console.log(that.data.hotComments);
+        }
+        
+      }
+    })
+  },
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function(options) {
-        let mvId = options.id;
-        console.log("MVID:" + mvId);
-        // 获取mv数据（播放地址做了防盗链处理，需单独获取[额，可用]）
-        this.getMvDetail(mvId);
-        // 获取mvUrl
-        this.getMvUrl(mvId)
-    },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function() {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function() {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function() {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function() {
-
+  // 查看或关闭简介
+  openOrCloseDesc: function(e) {
+    var isOpen = this.data.isOpen
+    if (isOpen) {
+      isOpen = false;
+    } else {
+      isOpen = true;
     }
+    this.setData({
+      isOpen
+    })
+    console.log(isOpen);
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    let mvId = options.id;
+    console.log("MVID:" + mvId);
+    // 获取mv数据（播放地址做了防盗链处理，需单独获取[额，可用]）
+    this.getMvDetail(mvId);
+    // 获取mvUrl
+    this.getMvUrl(mvId);
+    // 获取相关mv
+    this.getRelatedAllvideo(mvId);
+    // 获取评论
+    this.getComments(mvId);
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
 })
--- utils/util.js
+++ utils/util.js
@@ -40,7 +40,9 @@ function parse_lrc(lrc_content) {
   let scroll = true;
   for (let i in lrc_row) {
     if ((lrc_row[i].indexOf(']') == -1) && lrc_row[i]) {
-      now_lrc.push({ lrc: lrc_row[i] });
+      now_lrc.push({
+        lrc: lrc_row[i]
+      });
     } else if (lrc_row[i] != "") {
       var tmp = lrc_row[i].split("]");
       for (let j in tmp) {
@@ -49,15 +51,18 @@ function parse_lrc(lrc_content) {
         tmp2 = tmp2.split(":");
         let lrc_sec = parseInt(tmp2[0] * 60 + tmp2[1] * 1);
         if (lrc_sec >= 0) {
-        // if (lrc_sec && (lrc_sec > 0)) {
+          // if (lrc_sec && (lrc_sec > 0)) {
           let lrc = (tmp[tmp.length - 1]).replace(/(^\s*)|(\s*$)/g, "");
-          lrc && now_lrc.push({ lrc_sec: lrc_sec, lrc: lrc });
+          lrc && now_lrc.push({
+            lrc_sec: lrc_sec,
+            lrc: lrc
+          });
         }
       }
     }
   }
   if (!scroll) {
-    now_lrc.sort(function (a, b) {
+    now_lrc.sort(function(a, b) {
       return a.lrc_sec - b.lrc_sec;
     });
   }
@@ -67,9 +72,140 @@ function parse_lrc(lrc_content) {
   };
 }
 
+// 时间处理
+function formatTimeCommit(date, type) {
+  type = type || 1;
+  //type 1,完成输出年月日时分秒，2对比当前时间输出日期，或时分;
+  var d = new Date(date)
+  var year = d.getFullYear()
+  var month = d.getMonth() + 1
+  var day = d.getDate()
+  var hour = d.getHours()
+  var minute = d.getMinutes()
+  var second = d.getSeconds();
+  if (type == 1) {
+    return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':');
+  } else if (type == 3) {
+    return [year, month, day].map(formatNumber).join('-');
+  } else {
+    var jm = new Date,
+      Fo = jm.getTime() - date;
+    if (Fo <= 6e4)
+      return "刚刚";
+    var Qq = jm.getHours() * 36e5 + jm.getMinutes() * 6e4 + jm.getSeconds() * 1e3;
+    if (day == jm.getDate()) {
+      if (Fo < 36e5) {
+        var bOh = Math.floor(Fo / 6e4);
+        return bOh + "分钟前"
+      }
+      return [hour, minute].map(formatNumber).join(':');
+    } else {
+      if (Fo < Qq + 864e5) {
+        return "昨天" + [hour, minute].map(formatNumber).join(':');
+      } else {
+        var hq = jm.getFullYear(),
+          bOg = new Date(hq, 0, 1);
+        var Qq = jm.getTime() - bOg.getTime();
+        if (Fo < Qq) {
+          return year + "年" + month + "月" + day + "日" + [hour, minute].map(formatNumber).join(':');
+        }
+        return year + "年" + month + "月" + day + "日"
+      }
+    }
+  }
+}
+
+// 评论表情处理
+function emoji(str) {
+  if (!str) return;
+  var bl = {
+    "大笑": "86",
+    "可爱": "85",
+    "憨笑": "359",
+    "色": "95",
+    "亲亲": "363",
+    "惊恐": "96",
+    "流泪": "356",
+    "亲": "362",
+    "呆": "352",
+    "哀伤": "342",
+    "呲牙": "343",
+    "吐舌": "348",
+    "撇嘴": "353",
+    "怒": "361",
+    "奸笑": "341",
+    "汗": "97",
+    "痛苦": "346",
+    "惶恐": "354",
+    "生病": "350",
+    "口罩": "351",
+    "大哭": "357",
+    "晕": "355",
+    "发怒": "115",
+    "开心": "360",
+    "鬼脸": "94",
+    "皱眉": "87",
+    "流感": "358",
+    "爱心": "33",
+    "心碎": "34",
+    "钟情": "303",
+    "星星": "309",
+    "生气": "314",
+    "便便": "89",
+    "强": "13",
+    "弱": "372",
+    "拜": "14",
+    "牵手": "379",
+    "跳舞": "380",
+    "禁止": "374",
+    "这边": "262",
+    "爱意": "106",
+    "示爱": "376",
+    "嘴唇": "367",
+    "狗": "81",
+    "猫": "78",
+    "猪": "100",
+    "兔子": "459",
+    "小鸡": "450",
+    "公鸡": "461",
+    "幽灵": "116",
+    "圣诞": "411",
+    "外星": "101",
+    "钻石": "52",
+    "礼物": "107",
+    "男孩": "0",
+    "女孩": "1",
+    "蛋糕": "337",
+    18: "186",
+    "圈": "312",
+    "叉": "313"
+  }
+  var emojiObjs = [];
+  str = str.replace(/\[([^\[\]]+)\]/g, ':$1:')
+  var eReg = new RegExp("[:]");
+  var array = str.split(eReg);
+  for (var i = 0; i < array.length; i++) {
+    var ele = array[i];
+    var emojiObj = {};
+    if (bl[ele]) {
+      emojiObj.node = "element";
+      emojiObj.tag = "emoji";
+      emojiObj.text = bl[ele];
+    } else {
+      emojiObj.node = "text";
+      emojiObj.text = ele;
+    }
+    emojiObjs.push(emojiObj);
+  }
+
+  return emojiObjs;
+}
+
 module.exports = {
   formatTime: formatTime,
+  formatTimeCommit: formatTimeCommit,
   trim: trim,
   toggleplay: toggleplay,
   parse_lrc: parse_lrc,
+  emoji: emoji,
 }
